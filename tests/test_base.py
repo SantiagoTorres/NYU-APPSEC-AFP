dff7f3cc018159d495b7035ff583ccc8a97dee6a
Santiago Torres <torresariass@gmail.com>
2014-09-11 00:10:53 -0400

    Adds test for bare connections and empty template

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,7 @@
 import unittest
+import socket
+
+import server
 
 # runs the baseline software tests:
 #       * opening and closing a socket
@@ -7,11 +10,37 @@ import unittest
 #       * testing for CLEAR methods
 #       * testing for QUIT methods
 #       * running the example sequence
-# 
 class test_base(unittest.TestCase):
 
+    # a placeholder to test the unittest framework
     def test_dummy_test(self):
         self.assertTrue(True)
+
+    # tests whether the connection is alive
+    def test_open_close(self):
+        afpserver = server.afp_server(10)
+        afpserver.start()
+       
+        print("connecting 11 times...")
+        for i in range(1,11):
+            client = socket.create_connection(('127.0.0.1',
+                afpserver.port))
+            client.close()
+        print("done")
+
+    # tests the put scenarios, different lengths, etc
+    def test_put(self):
+        pass
+
+    # tests the get methods, put occurs  here also
+    def test_get(self):
+        pass
+
+    # tests the clear methods, put occurrs here also
+    def test_clear(self):
+        pass
+
+
 
 
 def base_suite():
